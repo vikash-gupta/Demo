28980e513f1c380bbc9fbd7c6f2a4b83a1a8abd8
Vikash Gupta <vikash.gupta@outlook.com>
2012-09-18 03:58:06 +0000

    Revert "Revert "First Commit""
    This reverts commit 75cf52d9b047cc19a9097322adea3475479da7af.

---
+++ Web/Controllers/AccountsController.cs
@@ -0,0 +1,46 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using References;
+
+namespace Web.Controllers
+{
+    public class AccountsController : ApiController
+    {
+        public IAccountsRepository AccountsRepository { get; set; }
+        public AccountsController(IAccountsRepository repository)
+        {
+            AccountsRepository = repository;
+        }
+        // GET api/accounts
+        public IEnumerable<Account> Get()
+        {
+            return AccountsRepository.GetAccounts();
+        }
+
+        // GET api/accounts/5
+        public Account Get(int id)
+        {
+            return AccountsRepository.GetAccount(id);
+        }
+
+        // POST api/accounts
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/accounts/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/accounts/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
