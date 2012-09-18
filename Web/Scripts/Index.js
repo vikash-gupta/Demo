25cbe449dc98c824734c39c28610b6df4a185d0f
Vikash Gupta <vikash.gupta@outlook.com>
2012-09-18 03:50:13 +0000

    First Commit
    Added Samm.JS for SPA navigation; Autofac Web API Integration for IoC

---
+++ Web/Scripts/Index.js
@@ -0,0 +1,40 @@
+﻿$(function () {
+    function AccountViewModel() {
+        // Data
+        var self = this;
+        self.chosenAccountDetails = ko.observable();
+        self.accountsSummary = ko.observable();
+
+        // Behaviours    
+        self.goToAccount = function (account) { location.hash = "api/accounts/"+account.Id };
+
+        // Client-side routes    
+        Sammy(function () {
+            this.get('#:summary', function () {
+                self.chosenAccountDetails(null);
+                $.get("/api/accounts", self.accountsSummary);
+            });
+            this.get('#api/accounts/:accountId', function () {
+                self.accountsSummary(null);
+                $.get("/api/accounts/100" + this.params.accountId, self.chosenAccountDetails);
+            });
+
+
+
+            this.get('', function () { this.app.runRoute('get', '#summary') });
+
+        }).run();
+    };
+    ko.applyBindings(new AccountViewModel());
+});
+
+
+//$(function () {
+//    var resultsFromWebApi = $.get('api/accounts', function (data) {
+//        function MyViewModel() {
+//            this.accounts = data;
+//        }
+//        ko.applyBindings(new MyViewModel());
+//    });
+//    //ko.applyBindings(new AccountViewModel());
+//});
