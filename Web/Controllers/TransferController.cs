d99082031cbf554a2ef84bae4111908bbd84ea49
Vikash Gupta <vikash.gupta@outlook.com>
2012-09-26 12:21:31 +0000

    WebAPI exposes PUT methods
    Replaced POST with PUT.

--- Web/Controllers/TransferController.cs
+++ Web/Controllers/TransferController.cs
@@ -11,18 +11,17 @@ namespace Web.Controllers
     //[Authorize]
     public class TransferController : ApiController
     {
-        public string Post(Transfer transferModel)
+        public string Put(Transfer transferModel)
         {
-            if(!ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
                 var values = ModelState.Values;
-                foreach(var value in values)
+                foreach (var value in values)
                 {
                     return value.Errors[0].ErrorMessage;
                 }
             }
             return "Data posted successfully";
-
         }
     }
 }
--- Web/Scripts/Transfer.js
+++ Web/Scripts/Transfer.js
@@ -1,14 +1,25 @@
 ﻿$(function () {
     $("#form1").submit(function () {
-        var jqxhr = $.post('api/transfer', $('#form1').serialize())
-            .success(function (a) {
+        var isValid = $("#form1").valid();
+        if (!isValid) {
+            return false;
+        }
+        $.ajax({
+            type: "PUT",
+            url: "api/transfer",
+            data: $("#form1").serialize(),
+            success: function (a) {
                 $('#message').html(a);
                 $("#form1").hide();
-            })
-            .error(function (a) {
+            },
+            error: function (a) {
                 $('#message').html(a.responseText);
                 $("#form1").hide();
-            });
+            }
+        });
+        
         return false;
     });
+    $("#form1").validate();
+
 });
